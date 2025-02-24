7c56489c337df86d5984da5b3f059059d8d841c6
Sneha Purkayastha <iamsneha0296@gmail.com>
2025-02-24 15:16:13 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -4,39 +4,18 @@ import Footer from "./components/Footer";
 import NavbarBuyer from "./components/NavbarBuyer";
 import NavbarSeller from "./components/NavbarSeller";
 import SellerDashboard from "./pages/SellerDashboard/SellerDashboard";
-import EditGig from "./pages/EditGigPage/EditGig";
 import BuyersProjects from "./pages/BuyersHomepage/BuyersProjects";
 import BuyersFreelancers from "./pages/BuyersHomepage/BuyersFreelancers";
-import Description from "./pages/EditGigPage/Description";
-import Messages from "./pages/MessagePage/Messages";
-import Settings from "./pages/SettingsPage/Settings";
 import AboutUs from "./pages/AboutUs.js/AboutUs";
 import ContactUs from "./pages/ContactUs.js/ContactUs";
 import PrivacyPolicy from "./pages/PrivacyPolicy.js/PrivacyPolicy";
 import TermsAndConditions from "./pages/TermsAndConditions.js/TermsAndConditions";
 import Maintenance from "./pages/Maintenance/Maintenance";
-import BillingAndPayment from "./pages/Profile/BillingAndPayment";
-import PricingPlan from "./pages/PricingPlan/PricingPlan";
 import Gig from "./pages/GigPage/Gig";
-import PaymentMethodFilling from "./pages/SettingsPage/PaymentMethodFilling";
-import HelpCenter from "./pages/Profile/HelpCenter";
-import Orders from "./pages/Business/Orders";
-import IndividualOrder from "./components/IndividualOrder";
-import Growth from "./pages/Business/Growth";
-import TotalEarning from "./pages/Business/TotalEarning";
-import Withdrawal from "./pages/Business/Withdrawal";
-import LevelOverview from "./pages/Business/LevelOverview";
 import SellerProfile from "./pages/Profile/SellerProfile";
 import BuyerProfile from "./pages/Profile/BuyerProfile";
-// import BuyerProfileSellerPOV from "./components/BuyerProfileSellerPOV";
-import SellerJobs from "./pages/Jobs/SellerJobs";
-import BuyerJobs from "./pages/Jobs/BuyerJobs";
 import Users from "./components/Users";
-import ApplyJobsSeller from "./pages/Jobs/ApplyJobsSeller";
-import ReferAFriend from "./pages/Profile/ReferAFriend";
-import CashFree from "./pages/SettingsPage/CashFree";
 import AIChatBot from "./components/AIChatBot";
-// import BuyerSidebarSellerPOV from "./components/BuyerSidebarSellerPOV";
 import BuyerProfileSellerPOV from "./pages/Profile/BuyerProfileSellerPOV";
 import BillingHistory from "./components/BillingHistory";
 
@@ -83,37 +62,18 @@ const AppWithMode = () => {
           path: "/",
           element: mode === "BUYER" ? <BuyersProjects /> : <SellerDashboard />,
         },
-        { path: "/editGig", element: <EditGig /> },
-        { path: "/payment", element: <PaymentMethodFilling /> },
-        { path: "/description", element: <Description /> },
         { path: "/maintenance", element: <Maintenance /> },
-        { path: "/messages", element: <Messages /> },
-        { path: "/settings", element: <Settings /> },
         { path: "/buyersfreelancers", element: <BuyersFreelancers /> },
         { path: "/sellerdashboard", element: <SellerDashboard /> },
         { path: "/aboutus", element: <AboutUs /> },
         { path: "/contactus", element: <ContactUs /> },
         { path: "/privacypolicy", element: <PrivacyPolicy /> },
         { path: "/termsandconditions", element: <TermsAndConditions /> },
-        { path: "/billingAndPayment", element: <BillingAndPayment /> },
         { path: "/billinghistory", element: <BillingHistory /> },
-        { path: "/pricingPlan", element: <PricingPlan /> },
         { path: "/gig", element: <Gig /> },
-        { path: "/helpcenter", element: <HelpCenter /> },
-        { path: "/orders", element: <Orders /> },
-        { path: "/individualorder", element: <IndividualOrder /> },
-        { path: "/growth", element: <Growth /> },
-        { path: "/totalearning", element: <TotalEarning /> },
-        { path: "/withdrawal", element: <Withdrawal /> },
-        { path: "/leveloverview", element: <LevelOverview /> },
         { path: "/profileSeller", element: <SellerProfile /> },
         { path: "/profileBuyer", element: <BuyerProfile /> },
-        { path: "/buyerjob", element: <BuyerJobs /> },
-        { path: "/sellerjob", element: <SellerJobs /> },
         { path: "/users", element: <Users /> },
-        { path: "/applyjobs", element: <ApplyJobsSeller /> },
-        { path: "/referafriend", element: <ReferAFriend /> },
-        { path: "/cashfree", element: <CashFree /> },
         { path: "/ai", element: <AIChatBot /> },
         { path: "/sellerPOV", element: <BuyerProfileSellerPOV /> },
       ],
--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,26 @@
 // src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, RecaptchaVerifier, signInWithPhoneNumber,FacebookAuthProvider } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getAuth, GoogleAuthProvider, RecaptchaVerifier, signInWithPhoneNumber,FacebookAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAddsSBE78uaHD6VN0wsjrRCGFpfyEYOV4",
-  authDomain: "fincust-ae2ee.firebaseapp.com",
-  projectId: "fincust-ae2ee",
-  storageBucket: "fincust-ae2ee.firebasestorage.app",
-  messagingSenderId: "52989738434",
-  appId: "1:52989738434:web:62fb0b80269e25b5606a5b",
-  measurementId: "G-63LXHR6SMW"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAddsSBE78uaHD6VN0wsjrRCGFpfyEYOV4",
+//   authDomain: "fincust-ae2ee.firebaseapp.com",
+//   projectId: "fincust-ae2ee",
+//   storageBucket: "fincust-ae2ee.firebasestorage.app",
+//   messagingSenderId: "52989738434",
+//   appId: "1:52989738434:web:62fb0b80269e25b5606a5b",
+//   measurementId: "G-63LXHR6SMW"
+// };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-if (window.location.hostname === "localhost") {
-  auth.settings.appVerificationDisabledForTesting = true;
-}
-const googleProvider = new GoogleAuthProvider();
-const facebookProvider = new FacebookAuthProvider();
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// if (window.location.hostname === "localhost") {
+//   auth.settings.appVerificationDisabledForTesting = true;
+// }
+// const googleProvider = new GoogleAuthProvider();
+// const facebookProvider = new FacebookAuthProvider();
 
-export { auth, googleProvider, RecaptchaVerifier, signInWithPhoneNumber,facebookProvider };
+// export { auth, googleProvider, RecaptchaVerifier, signInWithPhoneNumber,facebookProvider };
 
 
 // // Import the functions you need from the SDKs you need
